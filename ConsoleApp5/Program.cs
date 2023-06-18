25363601649b1487a0a21bd0c06c7fb38998bb5d
Xtrat3 <Xtrat3@XTRAT3>
2023-06-19 00:42:30 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿//1
+Console.Write("Введите строку чисел: "); 
+string str = Console.ReadLine()!;
+string[] mas = str.Split(',');
+int sum = 0;
+foreach (string i in mas)
+{
+    sum += int.Parse(i);
+}
+Console.WriteLine("S=" + sum);
+//2
+Console.Write("Введите предложение: "); 
+string str2 = Console.ReadLine()!;
+string[] mas2 = str2.Split(' '); 
+int count = 0;
+foreach (string i in mas2)
+{
+    count ++;
+}
+
+Console.WriteLine("S=" + count);
